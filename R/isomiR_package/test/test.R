63a39bd992543175675d68b005e02f84eed10139
Lorena Pantano <lorena.pantano@gmail.com>
2014-02-24 13:13:22 +0100

    cleaning code to get check return code 0

---
+++ R/isomiR_package/test/test.R
@@ -0,0 +1,22 @@
+#seq path to source code
+files<-c("y0d2.hsa.fa.ad.new.mirna",
+          "y0d34.hsa.fa.ad.new.mirna",
+         "y66d0.hsa.fa.ad.new.mirna",
+         "y80d0.hsa.fa.ad.new.mirna"
+         )
+
+library(isomiRs)
+d<-data.frame(condition=c("p","p","c","c"))
+row.names(d)<-paste(d[,1],1:2,sep="")
+
+obj<-loadIso2(files,d,skip=0,header=T)
+#check plot iso
+obj<-plotIso(obj,type="sub")
+
+#check diff exp: this will become a DESeq2 obj,
+#so any function can be apply to this
+dds<-deIso(obj,formula=~condition,ref=T)
+#plotMA
+library(DESeq2)
+plotMA(dds)
+
