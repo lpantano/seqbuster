9585018c020e6d2dd920e22d162efeff005c4f79
lpantano <lorena.pantano@gmail.com>
2014-04-17 18:24:59 +0200

    new examples

--- R/isomiR_package/test/test.R
+++ R/isomiR_package/test/test.R
@@ -6,17 +6,19 @@ library(isomiRs)
 #seq path to source code
 files<-c("y0d2.hsa.fa.ad.new.mirna",
           "y0d34.hsa.fa.ad.new.mirna",
+         "y0d204.hsa.fa.ad.new.mirna",
          "y66d0.hsa.fa.ad.new.mirna",
-         "y80d0.hsa.fa.ad.new.mirna"
+         "y80d0.hsa.fa.ad.new.mirna",
+         "y88d0.hsa.fa.ad.new.mirna"
          )
 
 
-d<-data.frame(condition=c("p","p","c","c"))
-row.names(d)<-paste(d[,1],1:2,sep="")
+d<-data.frame(condition=c("nb","nb","nb","o","o","o"))
+row.names(d)<-paste(d[,1],1:3,sep="")
 
 obj<-loadIso(files,d,skip=0,header=T)
 #check plot iso
-obj<-plotIso(obj,type="sub")
+obj<-plotIso(obj,type="t5")
 
 #check diff exp: this will become a DESeq2 obj,
 #so any function can be apply to this
@@ -29,5 +31,5 @@ head(counts(dds))
 
 #get counts
 counts<-makeCounts(obj)
-isoPLSDA(obj)
-
+pls.obj<-isoPLSDA(obj)
+isoPLSDAplot(pls.obj$components,obj@design[,1])
