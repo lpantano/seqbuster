d564417631e51bb87bd5ac2544f3fafdcfdac3c1
lpantano <lorena.pantano@gmail.com>
2014-04-24 10:51:03 +0200

    new vignette, and data

--- R/isomiR_package/isomiRs/R/internal.R
+++ R/isomiR_package/isomiRs/R/internal.R
@@ -18,8 +18,12 @@ filter.by.cov<-function(table,limit=10){
   freq=NULL
   tab.fil<-table[table$DB=="miRNA",]
   tab.fil.out<-data.table(tab.fil)
-  tab.fil.out<-as.data.frame(tab.fil.out[,list(total=sum(freq)),by=c('mir')])
-  tab.fil<-merge(tab.fil[,c(3,1:2,4:ncol(tab.fil))],tab.fil.out,by=1)
+  tab.fil.out<-as.data.frame(tab.fil.out[,list(total=sum(freq)),
+                                         by=c('mir')])
+  #tab.fil.out<-ddply(tab.fil.out,"mir",summarise,
+  #                   total=length(mir))
+  tab.fil<-merge(tab.fil[,c(3,1:2,4:ncol(tab.fil))],tab.fil.out,
+                 by=1)
   tab.fil$score<-tab.fil$freq/tab.fil$total*100
   tab.fil<-tab.fil[tab.fil$score>=limit,]
   return (tab.fil)
@@ -32,7 +36,7 @@ filter.by.cov<-function(table,limit=10){
 #' @param cov remove sequences that have relative abundance lower than this number
 filter.table<-function(table,cov=10){
   
-  
+  #print(date())
   table<-put.header(table)
   table<-filter.by.cov(table,cov)
   return(table)
@@ -136,6 +140,7 @@ collapse.mirs<-function(table,ref=F,iso5=F,iso3=F,add=F,mism=F,seed=F){
 #' @param table object miraligner table
 #' @param colid column to be considered
 isomir.position<-function(table,colid){
+  #print(date())
   temp<-table
   temp[,colid]<-as.character(temp[,colid])
   pos<-as.data.frame(t(as.data.frame((strsplit(temp[,colid],"-",fixed=2)))))
