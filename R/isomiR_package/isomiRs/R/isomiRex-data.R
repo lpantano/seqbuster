3a913bccfbc0e55f6e4218691a124e68b029becd
Lorena <lorena.pantano@gmail.com>
2014-06-26 12:26:12 -0400

    remove source

--- R/isomiR_package/isomiRs/R/isomiRex-data.R
+++
@@ -1,8 +0,0 @@
-#' @name isomiRex
-#' @title isomirDataSeq
-#' @description This data set is the object return by \code{loadIso}
-#' @docType data
-#' @usage isomiRex
-#' @format a \code{isomirDataSeq} instance, 1 row per CpG island.
-#' @author Lorena Pantano, 2014-04-08
-NULL
--- R/isomiR_package/isomiRs/inst/unitTests/ test_singleFunctions.R
+++
@@ -1,8 +0,0 @@
-test_singleFunctions <-
-    function()
-    {
-        data(isomiRs)
-        checkTrue(class(degMean(DEGreportSet$deg[,4],
-                DEGreportSet$counts))[[2]]=="ggplot")
-
-    }
